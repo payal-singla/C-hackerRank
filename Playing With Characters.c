03f3d6d8eb3a11cb2e7bd4acd358b80d204382e9
Payal Singla <singlapayal43@gmail.com>
2020-08-29 15:45:52 +0530

    Update Playing With Characters.c

--- Playing With Characters.c
+++ Playing With Characters.c
@@ -41,3 +41,21 @@ Sample Output 0
 C
 Language
 Welcome To C!! */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+  char ch,s[50],sen[1000];
+  scanf("%c",&ch);
+  scanf("%s",&s);
+  scanf("\n");
+  scanf("%[^\n]%*c",&sen) ;
+  printf("%c\n",ch);
+  printf("%s\n",s);
+  printf("%s",sen);
+    return 0;
+}
